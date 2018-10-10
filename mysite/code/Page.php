daaa931d94355dbedb335be4f06d0f4ede73b110
Jessica Brown <jessica-e-brown@uiowa.edu>
2018-10-10 09:55:35 -0500

    upgrade to ss4

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,60 +1,12 @@
 <?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
 class Page extends SiteTree {
 
 	private static $db = array(
-		
 	);
 
 	private static $has_one = array(
 	);
-
-
-	private static $many_many = array (
-	);
-
-    private static $many_many_extraFields=array(
-
-    );
-
-    private static $plural_name = "Pages";
-
-	private static $defaults = array (
-
-	);
-
-
-	public function getCMSFields(){
-		$f = parent::getCMSFields();
-
-		return $f;
-	}
-	
-}
-class Page_Controller extends ContentController {
-
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array (
-	);
-
-	public function init() {
-		parent::init();
-
-
-	}
-
-
 }
