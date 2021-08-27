2d25c411c5ce726de8ce165428d2e538f3f261f8
Dustin Quam <quamsta@gmail.com>
2021-08-27 13:50:43 -0500

    updates

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,7 +3,8 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Control\Director;
-
+use SilverStripe\Forms\HTMLEditor\HtmlEditorConfig;
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 // remove PasswordValidator for SilverStripe 5.0
 $validator = new PasswordValidator();
 
@@ -14,3 +15,5 @@ Member::set_password_validator($validator);
 if(Director::isLive()) {
 	Director::forceSSL();
 }
+
+TinyMCEConfig::get('cms')->setOption('entities', '160,nbsp,162,cent,8364,euro,163,pound,91');
